15096d5652c33a17796e48547a6b5a244cbf0595
Eric Vaughn <eric.vaughn41@gmail.com>
2011-09-17 22:45:01 -0700

    first commit for new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Madfortunate::Application.config.secret_token = 'c066f3d7c4c79e5e915ad01b8a7987decf04b925514788cc7c0f503c748045cfd3ac62ac8944abb72adaa55273e01ef9960db2e55c9c43eb34ad634e732792e3'
